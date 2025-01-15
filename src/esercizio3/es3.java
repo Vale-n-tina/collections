a10278fb14ac45a233bb8fe18d1f00d04662bc59
Vale-n-tina <valentia.grg@gmail.com>
2025-01-15 09:56:22 +0100

    .

--- src/esercizio3/es3.java
+++ src/esercizio3/es3.java
@@ -5,10 +5,10 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class es3 {
-
+	private static Map<String, String> rubricaTelefonica = new HashMap<>();
 
 	public static void inserimentoCoppia() {
-		Map<Integer, String> rubricaTelefonica = new HashMap<>();
+
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.println("Quanti numeri teelfonici vuoi salvare?");
@@ -18,11 +18,40 @@ public class es3 {
 			System.out.println("Inserisci il nome");
 			String nome = scanner.nextLine();
 			System.out.println("Inserisci il numero di telefono ");
-			int numeroTelefono = scanner.nextInt();
-			scanner.nextLine();
-			rubricaTelefonica.put(numeroTelefono, nome);
+			String numeroTelefono = scanner.nextLine();
+
+			rubricaTelefonica.put(nome, numeroTelefono);
 		}
 		System.out.println("Rubrica : " + rubricaTelefonica);
 
 	}
+
+	public static void rimuovi(String nome){
+		 rubricaTelefonica.remove(nome);
+	}
+
+	public static void trovaPErsonaByTelefono(String telefono){
+		for(String nome:rubricaTelefonica.keySet()){
+			if(telefono.equals(rubricaTelefonica.get(nome))){
+				System.out.println("telefono trovato per " + nome);
+				return;
+			}
+		}
+	}
+
+	public static void trovaTelefonoByPerona(String nome){
+		String telefono=rubricaTelefonica.get(nome);
+
+		if(telefono!=null){
+			System.out.println(telefono);
+		}else{
+			System.out.println("persona non presente");
+		}
+	}
+	public static void stampa(){
+		for(String nome: rubricaTelefonica.keySet()){
+			System.out.println("nome" + nome + ", telefono: " + rubricaTelefonica.get(nome));
+		}
+	}
+
 }
